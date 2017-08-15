5b32dde2044cafadc7edc85f89e34d89fad252b9
craigmcginley <craigmcginley@gmail.com>
2017-08-15 10:37:31 -0400

    Basic create-react-app with react-axe enabled   - Added input and h2 css which should both fail react-axe rules

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import axe from 'react-axe';
+
+if (process.env.NODE_ENV !== 'production') {
+	axe(React, ReactDOM, 1000);
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
